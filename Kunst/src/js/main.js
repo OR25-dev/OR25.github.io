4542bf4b185e8088f5322c622b445aa1c3f0015d
OR25 <jasper.moldenhauer@gymlgh.de>
2025-01-15 17:47:59 +0100

    Basic

---
+++ Kunst/src/js/main.js
@@ -0,0 +1,25 @@
+const text = "Erfindungen von Schüler*innen am Gymnasium Langenhagen!";
+const initialDelay = 500; // milliseconds
+const letterDelay = 50; // milliseconds
+
+let index = 0;
+
+function writeText() {
+  document.getElementById('text').innerText = text.slice(0, index);
+  index++;
+  
+  if(index <= text.length) {
+    setTimeout(writeText, letterDelay);
+  }
+}
+
+setTimeout(writeText, initialDelay);
+
+document.addEventListener("DOMContentLoaded", function() {
+  const hamburgerMenu = document.getElementById('hamburger-menu');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburgerMenu.addEventListener('click', function() {
+      navLinks.classList.toggle('show');
+  });
+});

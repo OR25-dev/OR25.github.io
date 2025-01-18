c64742827f8b1109d0f77de3b7dc28fcf2eb0114
OR25-dev <jasper.moldenhauer@gymlgh.de>
2025-01-18 13:16:41 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Szene, Kamera und Renderer initialisieren
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ alpha: true });  // Hintergrund transparent machen
